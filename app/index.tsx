c560f50b69a1d9eac161331d5906ad6ee02dd842
Venícios Vasconcelos <venicios.vasconcelos@academico.ifpb.edu.br>
2025-04-29 11:33:45 -0300

    Algumas modificações

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,47 @@
 import { Text, View, StyleSheet } from "react-native";
 
+const usuarios = [
+  {
+    id: 1,
+    nome: "Jurema",
+    cargo: "Aluna"
+  },
+  {
+    id: 2,
+    nome: "Jubileu",
+    cargo: "Personal"
+  },
+]
+
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <View style =
+      style={style.container}
+
     >
-      <Text >Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {
+      usuarios.map(item => (
+        <Text style = {{fontSize: 20}}>{item.nome}</Text>
+      ))
+      }
     </View>
   );
 }
 const style = StyleSheet.create ({
+ 
   container: {
     flex: 1,
     justifyContent: "flex-start",
     alignItems: "center",
+  }
+  itemLista: {
+    widht: '100%',
+    padding: 10,
+    marginBottom: 2,
+    backgroundColor: "blue"
+  },
+
+  textoLista:{
+    fontSize: 20
   }
 })
